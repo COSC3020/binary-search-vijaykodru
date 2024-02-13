f16077b3dffdc3c4a7ea5a3458f1fb599c2a71af
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-02-13 05:53:52 +0000

    Implemtation

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
 function binarySearch(list, element) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left <= right){
+        let middle = parseInt((left + right)/2)
+
+        if(arr[middle] === element){
+            return middle
+        }
+        else if(arr[middle] < element){
+            left = middle + 1
+        }
+        else{
+            right = middle - 1
+        }
+    }
+    
     return -1;
 }
+
+
+//Reference: The following website helped me understand how the binary search worked through a interactive excercise of implementing the search
+// https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
